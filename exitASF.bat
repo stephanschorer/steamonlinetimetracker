10be5c455fb10b77436a966e6b28a67abdae7b2e
Stephan <63855548+stephanschorer@users.noreply.github.com>
2023-07-08 19:54:17 +0200

    Delete exitASF.bat

--- exitASF.bat
+++
@@ -1,5 +0,0 @@
-::Starts the Task "SteamIdler" again for "listening" to the start events on steam.exe
-schtasks.exe /change /tn "SteamIdler" /enable
-
-::Stops ASF process
-taskkill /F /IM ArchiSteamFarm.exe
