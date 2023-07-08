05e2297b605ab9b45cb65e8b6eb8bf536ce201fd
Stephan <63855548+stephanschorer@users.noreply.github.com>
2023-07-08 19:54:22 +0200

    Delete runASF.bat

--- runASF.bat
+++
@@ -1,5 +0,0 @@
-::Stops the Task "SteamIdler" that the task is only run once
-schtasks.exe /change /tn "SteamIdler" /disable
-
-::Start ASF as your local user
-runas /user:pcname\username /savecred "cmd /c start /min C:\path\to\ArchiSteamFarm.exe"
